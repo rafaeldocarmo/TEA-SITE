c6b9b5198f2ed6f4ec62948b441d31afd517e361
Rafael Martinez do Carmo <110850303+rafaeldocarmo@users.noreply.github.com>
2024-08-18 22:44:23 -0300

    fix conflict atividades.js

--- front/src/mocks/atividades.js
+++ front/src/mocks/atividades.js
@@ -6,11 +6,7 @@ export const atividade = [
       objetivo: "Promover autonomia, possibilitar interação com o meio, estimular a independência funcional, explorar o ambiente, experimentar sensações, promover socialização com o mediador através do brincar.",
       materiais: "Cinco brinquedos simples, sem complexidade, selecionados previamente, podem ser deixados disponíveis para a escolha da criança. Os brinquedos podem ser selecionados conforme a habilidade a ser estimulada. Algumas opções de brinquedos são: bola, carrinho, chocalho, bonecos.",
       descricao: "Interação entre a criança e o mediador através do brincar. Ao chegar no ambiente terapêutico, prepare alguns brinquedos e deixe disponível para que a criança faça buscas e explore o ambiente, enquanto isso, o mediador interage com a criança e inicia um vínculo terapêutico para propor confiança.",
-<<<<<<< HEAD
       duracao: "5 minutos"
-=======
-      duracao: "5"
->>>>>>> ca43c3e413373c0e7d4392b903163da1a22db35e
     },
     {
       id: "02",
@@ -19,11 +15,7 @@ export const atividade = [
       objetivo: "Possibilitar aprendizagem motora, estimulação da coordenação motora, promover contato social e ensino de habilidades manuais.",
       materiais: "Utiliza-se um dos brinquedos disponíveis na atividade 1. A escolha inicial do brinquedo é de iniciativa da criança. No entanto, se a criança não o faz, a escolha é do mediador.",
       descricao: "Após a criança explorar o ambiente e se familiarizar, o mediador entra no jogo da criança e se torna parceiro de jogo. Para alcançar o objetivo, o mediador realiza imitação de atos motores simples até que a criança perceba e estabeleça interação, contato visual e brinque.",
-<<<<<<< HEAD
       duracao: "5 a 10 minutos"
-=======
-      duracao: "5 a 10"
->>>>>>> ca43c3e413373c0e7d4392b903163da1a22db35e
     },
     {
       id: "03",
@@ -32,11 +24,7 @@ export const atividade = [
       objetivo: "Estabelecer Contato Visual, promover troca social (interação), desenvolver a consciência da criança para expressões faciais e estratégias sociocomunicativas, aumentar a comunicação, otimizar a excitação da criança, estado e atenção.",
       materiais: "Muitas atividades de rotinas socais e sensoriais não envolvem nenhum objeto, apenas o corpo do mediador com a voz, gestos e movimentos. No entanto se a criança não corresponde, o mediador pode utilizar algum objeto de suporte para chamar a atenção como bolas de sabão, cata-vento ou objetos semelhantes que sejam interessantes para a criança.",
       descricao: "Atividade de Interação em conjunto em que a atenção da criança está focada no mediador, em vez de objetos. O mediador solicita a atenção da criança através de sons, gestos e movimentos com o objetivo de direcionar a atenção da criança para o rosto, voz, movimentos corporais e gestos do próprio mediador. A atividade é criada a partir da interação mediador-criança e varia conforme esse intervalo.",
-<<<<<<< HEAD
       duracao: "5 minutos"
-=======
-      duracao: "5"
->>>>>>> ca43c3e413373c0e7d4392b903163da1a22db35e
     },
     {
       id: "04",
@@ -45,11 +33,7 @@ export const atividade = [
       objetivo: "Melhorar a motricidade fina, estimular a criatividade, aumentar concentração e atenção, estimulação cognitiva, aprimorar as habilidades sociais e o contato visual.",
       materiais: "Papel A4, lápis de cor e giz de cera.",
       descricao: "A criança é colocada em uma mesa com cadeira para proporcionar um melhor alinhamento e organização corporal juntamente com o mediador. É ofertado lápis de cor, giz de cera e papel e a pintura inicialmente é livre e depois passa a ser direcionada. O mediador solicita que a criança faça desenhos específicos, formas geométricas e explore cores diversas. Com as crianças que não tem iniciativa, o mediador executa a pintura primeiro para estimular a criança a imitar, contudo, com algumas crianças que não tem iniciativa, o mediador realiza a pintura segurando e guiando a mão da criança até que ela alcance a independência funcional na atividade.",
-<<<<<<< HEAD
       duracao: "5 a 10 minutos "
-=======
-      duracao: "5 a 10"
->>>>>>> ca43c3e413373c0e7d4392b903163da1a22db35e
     },
     {
       id: "05",
@@ -58,11 +42,7 @@ export const atividade = [
       objetivo: "Estimular os sentidos táteis, visuais, cinestésicos e proprioceptivos, desenvolver coordenação motora, melhorar equilíbrio e noção espacial, estimulação visual, promover relaxamento, possibilitar interação social e aprimorar as habilidades sociais de brincar com o próximo.",
       materiais: "Piscina de Bolinhas.",
       descricao: "A criança é incentivada a entrar em uma piscina de bolinhas juntamente com o mediador e brincar livremente. A atividade também pode ser direcionada, solicitando que a criança encontre algum objeto ou que jogue as bolinhas em 1 cesto. Em casos de recusa, o mediador pode ajudar a criança a entrar ou até mesmo entrar primeiro para incentivar a criança.",
-<<<<<<< HEAD
       duracao: "5 a 10 minutos"
-=======
-      duracao: "5 a 10"
->>>>>>> ca43c3e413373c0e7d4392b903163da1a22db35e
     },
     {
       id: "06",
@@ -100,7 +80,6 @@ export const atividade = [
       id: "10",
       nome: "ENSINO DE IMITAÇÃO",
       slug: "ensino-de-imitacao",
-<<<<<<< HEAD
       objetivo: "Estimular a motricidade, a comunicação e a linguagem.",
       materiais: "Não há um recurso específico, o mediador deve aproveitar as oportunidades de interação com a criança.",
       descricao: "O ensino de imitação envolve diferentes estratégias em diversas atividades, a criança é estimulada a imitar gestos, expressões faciais e expressões vocais. São utilizados gestos comunicativos como acenar, apontar, esperar, colocar “aqui’, é por “ali” e assim é solicitado que a criança repita a ação. Também são utilizados brinquedos iguais para a criança e o mediador a fim de que a criança visualize a ação e tente repetir. ",
@@ -222,117 +201,3 @@ export const atividade = [
       duracao: "5 a 10 minutos",
     }
 ];
-
-
-=======
-      objetivo: "Estimular o contato visual e a interação social, desenvolver as habilidades motoras, promover a generalização e o aprendizado de novas habilidades através da observação e da imitação.",
-      materiais: "Brinquedos ou objetos que possam ser manipulados pelo mediador e pela criança.",
-      descricao: "A imitação pode ser um recurso poderoso para o aprendizado, principalmente em crianças pequenas. Nesta atividade, o mediador realiza ações simples e incentivam a criança a imitar. Os objetos e brinquedos podem ser utilizados para estimular essa imitação, como bater palmas, imitar sons, construir uma torre com blocos, etc. O mediador pode começar com imitações simples e progressivamente aumentar a complexidade das ações. O objetivo é que a criança comece a perceber o padrão e comece a imitar por conta própria, fortalecendo as conexões sociais e o aprendizado por observação.",
-    },
-    {
-      id: "11",
-      nome: "ESTIMULAÇÃO DE GESTOS NATURAIS",
-      slug: "estimulacao-de-gestos-naturais",
-      objetivo: "Desenvolver habilidades de comunicação não-verbal, como gestos e expressões faciais.",
-      materiais: "Objetos de interesse da criança que estejam fora de alcance.",
-      descricao: "Para incentivar a comunicação não-verbal, o mediador oferece à criança um objeto que ela não consegue alcançar diretamente. Isso força a criança a usar gestos ou expressões faciais para comunicar seu desejo. Com o tempo, a criança aprende a usar gestos naturais, como apontar ou pedir algo, de forma mais consciente e eficaz.",
-    },
-    {
-      id: "12",
-      nome: "APRENDIZADO DE HABILIDADES MOTORAS",
-      slug: "aprendizado-de-habilidades-motoras",
-      objetivo: "Desenvolver a coordenação motora fina e grossa, além de aumentar a independência funcional.",
-      materiais: "Atividades que envolvam movimento corporal, como pular, correr, ou manipular pequenos objetos.",
-      descricao: "O mediador cria um ambiente que desafia a criança a usar diferentes habilidades motoras. Por exemplo, pode-se incentivar a criança a pular em um pé só, correr entre cones, ou manipular objetos pequenos que exigem precisão. O objetivo é que a criança desenvolva maior controle sobre seus movimentos e ganhe confiança para realizar essas tarefas de forma independente.",
-    },
-    {
-      id: "13",
-      nome: "JOGO DE ESPELHOS",
-      slug: "jogo-de-espelhos",
-      objetivo: "Promover a autoconsciência e a compreensão das expressões faciais.",
-      materiais: "Espelho de mão ou espelho grande montado na parede.",
-      descricao: "O mediador usa um espelho para ajudar a criança a reconhecer e imitar expressões faciais e movimentos. A criança é incentivada a olhar para si mesma no espelho enquanto faz caretas, sorri ou finge estar triste. O mediador pode acompanhar a criança e fazer as mesmas expressões, reforçando o aprendizado e a autoconsciência.",
-    },
-    {
-      id: "14",
-      nome: "ATIVIDADES SENSORIAIS COM TEXTURAS",
-      slug: "atividades-sensoriais-com-texturas",
-      objetivo: "Estimular os sentidos táteis, desenvolvendo a sensibilidade e a consciência corporal.",
-      materiais: "Materiais com diferentes texturas, como tecidos, esponjas, areia, e argila.",
-      descricao: "O mediador prepara diferentes materiais com texturas variadas e incentiva a criança a tocar e explorar. A criança pode ser guiada a descrever as sensações, ajudando-a a associar palavras aos sentimentos táteis. Essa atividade também pode ser usada para relaxamento, se a criança demonstrar sensibilidade a certas texturas.",
-    },
-    {
-      id: "15",
-      nome: "JOGO DE PERGUNTAS E RESPOSTAS",
-      slug: "jogo-de-perguntas-e-respostas",
-      objetivo: "Aumentar a capacidade de comunicação verbal e a habilidade de responder adequadamente a perguntas.",
-      materiais: "Cartões com perguntas simples ou tópicos de conversa.",
-      descricao: "O mediador faz perguntas simples para a criança, que deve responder de forma apropriada. As perguntas podem variar desde 'Qual é a sua cor favorita?' até 'O que você fez ontem?'. O objetivo é estimular a criança a pensar e verbalizar suas respostas, melhorando suas habilidades de comunicação e expressão.",
-    },
-    {
-      id: "16",
-      nome: "CONSTRUÇÃO DE HISTÓRIAS",
-      slug: "construcao-de-historias",
-      objetivo: "Estimular a criatividade, o pensamento crítico, e as habilidades de comunicação.",
-      materiais: "Figuras de personagens, cenários, ou objetos que podem ser usados como inspiração para contar histórias.",
-      descricao: "A criança e o mediador colaboram para criar uma história, usando figuras ou objetos como inspiração. O mediador pode começar a história e encorajar a criança a continuar, ou deixar a criança liderar. Isso promove o desenvolvimento da narrativa, além de reforçar a capacidade da criança de seguir uma linha de pensamento e expressar ideias de forma clara.",
-    },
-    {
-      id: "17",
-      nome: "ESTIMULAÇÃO AUDITIVA COM MÚSICA",
-      slug: "estimulacao-auditiva-com-musica",
-      objetivo: "Desenvolver a percepção auditiva e a habilidade de seguir ritmos.",
-      materiais: "Instrumentos musicais simples, como tambores, chocalhos, ou sinos, e uma seleção de músicas.",
-      descricao: "O mediador toca diferentes tipos de música e incentiva a criança a reagir, seja dançando, batendo palmas, ou tocando um instrumento. A atividade pode ser estruturada para seguir ritmos ou simplesmente explorar sons. Isso ajuda a criança a desenvolver uma melhor percepção auditiva e a capacidade de sincronizar seus movimentos com a música.",
-    },
-    {
-      id: "18",
-      nome: "ATIVIDADES COM MASSINHA DE MODELAR",
-      slug: "atividades-com-massinha-de-modelar",
-      objetivo: "Estimular a criatividade, a coordenação motora fina, e a percepção tátil.",
-      materiais: "Massinha de modelar de diferentes cores.",
-      descricao: "A criança é incentivada a usar a massinha de modelar para criar diferentes formas e objetos. O mediador pode sugerir temas ou deixar a criança livre para criar o que quiser. Essa atividade também pode ser usada para ensinar cores, formas, e conceitos espaciais, enquanto a criança desenvolve suas habilidades motoras e táteis.",
-    },
-    {
-      id: "19",
-      nome: "EXPLORAÇÃO DA NATUREZA",
-      slug: "exploracao-da-natureza",
-      objetivo: "Estimular a curiosidade, a observação, e o respeito pelo meio ambiente.",
-      materiais: "Elementos naturais como folhas, pedras, flores, e gravetos.",
-      descricao: "O mediador leva a criança para um ambiente natural onde ela pode explorar e coletar diferentes elementos. A atividade pode incluir identificar plantas, observar insetos, ou simplesmente sentir diferentes texturas naturais. Essa exploração ajuda a criança a desenvolver uma conexão com a natureza e a aprender sobre o mundo ao seu redor.",
-    },
-    {
-      id: "20",
-      nome: "MONTAR QUEBRA-CABEÇAS",
-      slug: "montar-quebra-cabecas",
-      objetivo: "Desenvolver o raciocínio lógico, a percepção espacial, e a paciência.",
-      materiais: "Quebra-cabeças adequados à idade e ao nível de habilidade da criança.",
-      descricao: "A criança é incentivada a montar um quebra-cabeça, sozinha ou com a ajuda do mediador. Dependendo da complexidade do quebra-cabeça, o mediador pode oferecer dicas ou deixar a criança descobrir as soluções por conta própria. Isso ajuda a desenvolver habilidades de resolução de problemas e a capacidade de concentração.",
-    },
-    {
-      id: "21",
-      nome: "JOGO DA MEMÓRIA",
-      slug: "jogo-da-memoria",
-      objetivo: "Melhorar a memória visual e a concentração.",
-      materiais: "Cartas de memória com figuras.",
-      descricao: "O mediador organiza as cartas de memória viradas para baixo e desafia a criança a encontrar os pares correspondentes. Conforme a criança joga, ela vai melhorando sua capacidade de lembrar onde estão as cartas e desenvolvendo estratégias para ganhar. O jogo da memória é uma maneira divertida de melhorar a memória visual e a concentração.",
-    },
-    {
-      id: "22",
-      nome: "CONSTRUÇÃO COM BLOCOS",
-      slug: "construcao-com-blocos",
-      objetivo: "Estimular a criatividade, o raciocínio lógico, e a coordenação motora fina.",
-      materiais: "Blocos de construção de diferentes formas e tamanhos.",
-      descricao: "A criança é incentivada a usar blocos de construção para criar estruturas. O mediador pode sugerir construir uma torre, uma casa, ou qualquer outra coisa que a criança imaginar. Esta atividade ajuda a desenvolver habilidades de planejamento e a percepção espacial, enquanto a criança experimenta e aprende sobre equilíbrio e estrutura.",
-    },
-    {
-      id: "23",
-      nome: "CONTAÇÃO DE HISTÓRIAS",
-      slug: "contacao-de-historias",
-      objetivo: "Estimular a imaginação, a escuta ativa, e a capacidade de seguir narrativas.",
-      materiais: "Livros de histórias ou figuras ilustrativas.",
-      descricao: "O mediador lê uma história para a criança, incentivando-a a seguir a narrativa e a fazer perguntas. Dependendo da idade e da habilidade da criança, o mediador pode pedir para que ela repita partes da história ou para que invente um final alternativo. Esta atividade é excelente para desenvolver a compreensão auditiva e a criatividade.",
-    }
-];
->>>>>>> ca43c3e413373c0e7d4392b903163da1a22db35e
-  
